418bc6507bdcc149963881c5705f07ae9a729e1f
El_Granto <grant.sanger@gmail.com>
2016-02-01 19:24:34 -0500

    First commit. Yay! :D

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+  
+  var slideWidth = $('.slides article').width();
+  // $('.slides article').css({'height':slideWidth+'px'});
+  // $('.slides article a').css({'height':slideWidth+'px'});
+  // $('.slides article').css({'background-size':slideWidth+'px'});
+  
+  
+});
+
